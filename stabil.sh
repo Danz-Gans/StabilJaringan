f37c815f56a7a82aa50cc7160dab65248adaa130
Danz-Gans <85097166+Danz-Gans@users.noreply.github.com>
2021-06-06 01:31:57 +0700

    Add files via upload

---
+++ stabil.sh
@@ -0,0 +1,50 @@
+clear
+blue='\033[34;1m'
+green='\033[32;1m'
+purple='\033[35;1m'
+cyan='\033[36;1m'
+red='\033[31;1m'
+white='\033[37;1m'
+yellow='\033[33;1m'
+sleep 1
+echo
+toilet -f big -F gay Danz1337
+echo $green"My Website : https://tutorial-ngemhek.blogspot.com/"
+echo $green"My Team : GreyHat School Of Hackers - IndonesiaCyberNightmare"
+echo $green"Support : Mr.Wolverine & Keluarga"
+echo
+echo $red"Welcome To My Tools!!"
+echo
+sleep 2
+echo $red"Pilihan :"
+echo
+echo $red"1.) Stabilkan Jaringan"
+echo
+echo $red"0.) Keluar "
+echo
+echo $red
+read -p "Masukkan No : " bro
+
+if [ $bro = 1 ] || [ $bro = 1 ]
+then
+clear
+toilet -f big -F gay Danz1337
+echo $green"Stabilkan Jaringan By Danz1137"
+echo
+echo $green"My Website : https://tutorial-ngemhek.blogspot.com/"
+echo $green"My Team : GreyHat School Of Hackers - IndonesiaCyberNightmare>
+echo $green"Support : Mr.Wolverine & Keluarga"
+echo
+sleep 2
+ping -s1000 1.1.1.1
+fi
+
+if [ $bro = 0 ] || [ $bro = 0 ]
+then
+clear
+echo $green
+figlet "See Youu.."
+echo $green"Thankss"
+sleep 3
+exit
+fi
